6b850a7b51c2bfef17728e3613795dae2a92b4f0
Alex Lizano <alexsanty10lm@gmail.com>
2024-06-10 01:04:21 -0500

    Se construyeron los metodos para crear y modificar productos en el servicio correspondiente

--- src/main/java/com/grupo4/servicios/biller_project/services/ProductService.java
+++ src/main/java/com/grupo4/servicios/biller_project/services/ProductService.java
@@ -6,8 +6,13 @@ import java.util.Optional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.grupo4.servicios.biller_project.dtos.product.ProductCreateDto;
+import com.grupo4.servicios.biller_project.dtos.product.ProductUpdateDto;
+import com.grupo4.servicios.biller_project.entities.Category;
 import com.grupo4.servicios.biller_project.entities.Product;
 import com.grupo4.servicios.biller_project.repositories.CategoryRepository;
 import com.grupo4.servicios.biller_project.repositories.ProductRepository;
@@ -16,11 +21,24 @@ import com.grupo4.servicios.biller_project.repositories.ProductRepository;
 public class ProductService {
 
     private ProductRepository productRepository;
-    
+    private ModelMapper modelMapper;
+    private CategoryRepository categoryRepository;
+
     @Autowired
     public ProductService(ProductRepository productRepository, ModelMapper modelMapper,
             CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Product convertToEntity(ProductCreateDto productCreateDto) {
+        Product product = modelMapper.map(productCreateDto, Product.class);
+        Category category = categoryRepository.findById(productCreateDto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+
+        product.setCategory(category);
+        return product;
     }
 
     public List<Product> getAllProducts() {
@@ -33,6 +51,58 @@ public class ProductService {
             return (product.orElseThrow(() -> new Exception("El producto no existe")));
         } catch (Exception e) {
             throw new RuntimeException("El producto no existe");
+        }
+    }
+
+    public ResponseEntity<String> createProduct(ProductCreateDto productData) {
+        try {
+            Product product = convertToEntity(productData);
+            if (productRepository.existsByCode(productData.getCode())) {
+                throw new RuntimeException("El código del producto debe ser único");
+            }
+            productRepository.save(product);
+            return ResponseEntity.ok("El producto se creo con éxito");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.toString());
+        }
+    }
+
+    
+
+    public ResponseEntity<String> updateProduct(Long id, ProductUpdateDto productUpdateData) throws Exception {
+        try {
+            Product existingProduct = productRepository.findById(id)
+                    .orElseThrow(() -> new Exception("Product not found with id: " + id));
+
+            if (productUpdateData.getCategoryId() != null) {
+                Category category = categoryRepository.findById(productUpdateData.getCategoryId())
+                        .orElseThrow(() -> new Exception("Categoría no encontrada"));
+                existingProduct.setCategory(category);
+            }
+
+            if (productUpdateData.getName() != null) {
+                existingProduct.setName(productUpdateData.getName());
+            }
+
+            if (productUpdateData.getImage() != null) {
+                existingProduct.setImage(productUpdateData.getImage());
+            }
+
+            if (productUpdateData.getDescription() != null) {
+                existingProduct.setDescription(productUpdateData.getDescription());
+            }
+
+            if (productUpdateData.getUnitPrice() != null) {
+                existingProduct.setUnitPrice(productUpdateData.getUnitPrice());
+            }
+
+            if (productUpdateData.getStock() != null) {
+                existingProduct.setStock(productUpdateData.getStock());
+            }
+            productRepository.save(existingProduct);
+            return ResponseEntity.ok("El producto fue modificado con éxito");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.toString());
         }
     }
 }
