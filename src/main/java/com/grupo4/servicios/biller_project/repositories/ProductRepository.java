a8503b2ced5d1a5f1766971efc3c99ca0d26143c
Alex Lizano <alexsanty10lm@gmail.com>
2024-06-10 00:56:41 -0500

    Se crearon los repositorios para Products y Categories

--- src/main/java/com/grupo4/servicios/biller_project/repositories/ProductRepository.java
+++ src/main/java/com/grupo4/servicios/biller_project/repositories/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.grupo4.servicios.biller_project.repositories;
 
 import com.grupo4.servicios.biller_project.entities.Product;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByDeletedStatusFalse(Sort sort);
+    boolean existsByCode(String code);
 }
