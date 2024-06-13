e6f3f4aa78ad9ced752b429c75270de2b4e3e4e0
Jefferson Poaquiza <jeffstalim111@gmail.com>
2024-06-13 06:33:54 -0500

    El modulo auth contiene El AuthContrller, AuthResponse, AuthService, LoginRequest y el RegisterRequest para que se registre un usuario e inicie sesion en caso de no existir en la bd

---
+++ src/main/java/com/grupo4/servicios/biller_project/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.grupo4.servicios.biller_project.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String email;
+}
